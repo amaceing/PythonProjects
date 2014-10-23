d34cb5f1fd46f692e076beb2f3faf8a691924549
Anthony Mace <amace93@gmail.com>
2014-10-22 23:20:56 -0700

    implements bills in change

--- ChangeReturnProgram/changeReturn.py
+++ ChangeReturnProgram/changeReturn.py
@@ -1,1 +1,81 @@
 __author__ = 'anthonymace'
+
+def enter_cost_of_item():
+    cost = float(input("Enter cost: "))
+    print("Cost was: ", cost)
+    return cost
+
+def enter_payment_amount():
+    payment_amount = float(input("Enter payment amount: "))
+    print("Payment was: ", payment_amount)
+    return payment_amount
+
+def is_payment_enough(cost, payment):
+    return payment > cost
+
+def calc_difference(cost, payment):
+    return payment - cost
+
+def return_bills(difference):
+    hundreds = 0
+    fifties = 0
+    twenties = 0
+    tens = 0
+    ones = 0
+    while difference >= 100:
+        hundreds += 1
+        difference -= 100
+
+    while difference >= 50:
+        fifties += 1
+        difference -= 50
+
+    while difference >= 20:
+        twenties += 1
+        difference -= 20
+
+    while difference >= 10:
+        tens += 1
+        difference -= 10
+
+    while difference >= 1:
+        ones += 1
+        difference -= 1
+
+    return hundreds, fifties, twenties, tens, ones
+
+def return_coins(difference):
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
+
+    while difference >= 0.25:
+        quarters += 1
+        difference -= 0.25
+
+    while difference >= 0.1:
+        dimes += 1
+        difference -= 0.1
+
+    while difference >= 0.05:
+        nickels += 1
+        difference -= 0.05
+
+    while difference >= 0.01:
+        pennies += 1
+        difference -= 0.01
+
+    return quarters, dimes, nickels, pennies
+
+def main():
+    cost = enter_cost_of_item()
+    payment = enter_payment_amount()
+    if is_payment_enough(cost, payment):
+        difference = calc_difference(cost, payment)
+        bills = return_bills(difference)
+        change = return_coins(left_over_from_bills)
+        print(bills)
+        print(change)
+
+main()
